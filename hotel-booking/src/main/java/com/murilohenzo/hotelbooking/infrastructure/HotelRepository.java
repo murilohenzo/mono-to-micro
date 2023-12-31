ee445bcd99aa57c7b9853cb12c833663f16baf3b
murilohenzo <murilohezo@gmail.com>
2023-12-31 12:46:44 -0300

    feat(mono): Change folder monolith

--- hotel-booking/src/main/java/com/murilohenzo/hotelbooking/infrastructure/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.murilohenzo.hotelbooking.infrastructure;
-
-import com.murilohenzo.hotelbooking.domain.entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-}
